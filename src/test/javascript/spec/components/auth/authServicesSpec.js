cdd618c096ddf0bface2970079cf6ad3a252ca07
DGospodinov <gospodinovdenislav@gmail.com>
2015-05-05 15:30:15 +0300

    make it better

--- src/test/javascript/spec/components/auth/authServicesSpec.js
+++ src/test/javascript/spec/components/auth/authServicesSpec.js
@@ -13,18 +13,17 @@ describe('Services Tests ', function () {
             authService = Auth;
             spiedAuthServerProvider = AuthServerProvider;
             //Request on app init
-            $httpBackend.expectPOST(/api\/logout\?cacheBuster=\d+/).respond(200, ''); 
+            $httpBackend.expectPOST(/api\/logout\?cacheBuster=\d+/).respond(200, '');
 
             $httpBackend.expectGET('i18n/en/global.json').respond(200, '');
             $httpBackend.expectGET('i18n/en/language.json').respond(200, '');
-            $httpBackend.expectGET('scripts/components/navbar/navbar.html').respond({});
             $httpBackend.expectGET('i18n/en/global.json').respond(200, '');
             $httpBackend.expectGET('i18n/en/language.json').respond(200, '');
             $httpBackend.expectGET('i18n/en/main.json').respond(200, '');
             $httpBackend.expectGET('scripts/app/main/main.html').respond({});
-            
+
                 $httpBackend.expectGET(/api\/account\?cacheBuster=\d+/).respond({});
-            
+
           }));
         //make sure no expectations were missed in your tests.
         //(e.g. expectGET or expectPOST)
@@ -32,7 +31,7 @@ describe('Services Tests ', function () {
             $httpBackend.verifyNoOutstandingExpectation();
             $httpBackend.verifyNoOutstandingRequest();
         });
-        
+
         it('should call backend on logout then call authServerProvider.logout', function(){
             //GIVEN
             //Set spy
